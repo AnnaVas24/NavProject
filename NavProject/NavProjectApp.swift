44b597f27802aa1f0d2a2db56b5d88a7c92cf483
Anna <anna.vasi4ko@icloud.com>
2022-07-05 19:18:42 +0300

    Initial Commit

---
+++ NavProject/NavProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavProjectApp.swift
+//  NavProject
+//
+//  Created by Vasichko Anna on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
